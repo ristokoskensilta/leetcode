31e43f59e16fa03961ebcad242aee198eb5f081a
ristokoskensilta <90190249+ristokoskensilta@users.noreply.github.com>
2024-07-26 17:42:15 +0300

    Update leetcode-233-number-of-digit-one.js

--- leetcode-233-number-of-digit-one.js
+++ leetcode-233-number-of-digit-one.js
@@ -1,3 +1,6 @@
+// Leetcode problem 233. Number of Digit One
+// https://leetcode.com/problems/number-of-digit-one
+
 // Given an integer n, count the total number of digit 1 
 // appearing in all non-negative integers less than or equal to n.
 
