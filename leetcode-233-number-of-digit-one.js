36fbcae84eacd0bdca2015388493c3920609dec8
ristokoskensilta <90190249+ristokoskensilta@users.noreply.github.com>
2024-07-26 17:41:13 +0300

    Create leetcode-233-number-of-digit-one.js
    # Approach
    Given number $$n$$ is decreased to $$0$$ while counting occurrences of $$1$$. Thus digits $$d_n$$ are rolled to zero one by one starting from right. 
    ```
         n = ... | d_x | d_y | d_i | d_v | d_u | ... | d_0
                            /       \    
               LEFT PLACES | CURRENT | RIGHT PLACES
    ```
    For every digit $$d_i$$ and it's place value $$p_i$$ ($$= 10^i$$ i.e. base^index):
    - if $$d_i = 0$$ skip to the next place.
    - LEFT PLACES: Every digit $$1$$ of the number $$n$$ to left from the current position $$i$$ will occur $$d_i * p$$ times.
    - CURRENT PLACE: If the digit $$d_i$$ is $$1$$, it will be de decrease only ones; otherwise, digit $$1$$ will occur $$p_i$$ times.
    - RIGHT PLACES: There will be $$d * $$previously counted occurances of digit $$1$$ ($$=(prevOnes * base) + placeValue$$) to right from the current position.
     
    Sum occurances up and move to the next digit. 
    
    # Complexity
    - Time complexity:
    $$O(log(n))$$

---
+++ leetcode-233-number-of-digit-one.js
@@ -0,0 +1,27 @@
+// Given an integer n, count the total number of digit 1 
+// appearing in all non-negative integers less than or equal to n.
+
+var countDigitOne = function (n) {
+  let base = 10; 
+  let sum = 0;
+  const digits = Array.from(String(n), Number).reverse();
+  let oneCount = 0;     // digits 1 in the given number
+  for(const d of digits){
+    if(d == 1) 
+      oneCount++;
+  }
+  let placeValue = 1;   // current place value, ch. https://en.wikipedia.org/wiki/Positional_notation#Exponentiation
+  let prevOnes = 0;     // number of 1's in the less significant places 
+  for(const d of digits){
+    if(d > 0){
+      if(d == 1){
+        oneCount--;
+      }
+      //          LEFT PLACES              CURRENT PLACE           RIGHT PLACES
+      sum += (d*oneCount*placeValue) + (d > 1 ? placeValue : 1) + (d*prevOnes);
+    }
+    prevOnes = (prevOnes * base) + placeValue;
+    placeValue *= base;
+  }
+  return sum;
+};
